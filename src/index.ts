40a8a7da01f6d918402fb22a5d8edb4e517883ad
JPHuergo <135067225+JPHuergo@users.noreply.github.com>
2023-06-05 11:35:37 -0300

    agregue foto y cambie version de loader

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Loader, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -8,12 +8,21 @@ const app = new Application({
 	width: 640,
 	height: 480
 });
+Loader.shared.add({url:"./2.jpg", name:"placard"});
+Loader.shared.add({url:"./clampy.png", name:"Clampy"});
 
-const clampy: Sprite = Sprite.from("clampy.png");
+Loader.shared.onComplete.add(()=>{
 
-clampy.anchor.set(0.5);
 
-clampy.x = 300;
-clampy.y = 300;
+const clampy: Sprite = Sprite.from("placard");
+
+console.log("Hola Mundo!", clampy.width, clampy.height);
+
+//clampy.anchor.set(0.5);
+
+clampy.x = 0;
+clampy.y = 0;
 
 app.stage.addChild(clampy);
+});
+Loader.shared.load();
